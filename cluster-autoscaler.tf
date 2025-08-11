811cedb584883b455231aeb00401dffcf5a5def6
iamfet <fetuyee@gmail.com>
2025-08-11 18:31:25 +0000

    feat: add cluster autoscaler

--- cluster-autoscaler.tf
+++ cluster-autoscaler.tf
@@ -1,3 +1,16 @@
+provider "helm" {
+  kubernetes = {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+    exec = {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+    }
+  }
+}
+
 module "cluster_autoscaler_irsa" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
   version = "~> 5.59"
@@ -20,14 +33,13 @@ module "cluster_autoscaler_irsa" {
 }
 
 
-
 resource "helm_release" "cluster-autoscaler" {
   name       = "cluster-autoscaler"
   repository = "https://kubernetes.github.io/autoscaler"
   chart      = "cluster-autoscaler"
   version    = "9.48.0"
   namespace  = "kube-system"
-  depends_on = [module.eks, helm_release.aws-load-balancer-controller, module.cluster_autoscaler_irsa]
+  depends_on = [module.eks, module.cluster_autoscaler_irsa]
 
   set = [
     {
